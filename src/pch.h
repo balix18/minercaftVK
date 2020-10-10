0b731326f1d4a01e89bf845aab5acd664b7c772d
Balazs Oczot <balix18@gmail.com>
2020-10-10 12:23:54 +0200

    Helyes alignment

--- src/pch.h
+++ src/pch.h
@@ -19,6 +19,7 @@
 
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
 #include <glm/gtc/matrix_transform.hpp>
